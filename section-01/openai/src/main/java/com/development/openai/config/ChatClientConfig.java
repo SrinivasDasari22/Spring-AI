8ced8a49e1f7556b2e682901065b4fd504484c4d
Srinivas Dasari <dsaikrishna333@gmail.com>
2025-10-16 15:58:33 -0500

    Prompt Stuffing

--- section-01/openai/src/main/java/com/development/openai/config/ChatClientConfig.java
+++ section-01/openai/src/main/java/com/development/openai/config/ChatClientConfig.java
@@ -12,7 +12,15 @@ public class ChatClientConfig {
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder){
 
         return chatClientBuilder
-                .defaultSystem("you are java architect , apart from the java related question , don't give response to any other queries")
+                .defaultSystem("""
+                        You are an internal HR assistant. Your role is to help\s
+                        employees with questions related to HR policies, such as\s
+                        leave policies, working hours, benefits, and code of conduct.
+                        If a user asks for help with anything outside of these topics,\s
+                        kindly inform them that you can only assist with queries related to\s
+                        HR policies.
+                        """)
+                .defaultUser("How can you help me ?")
                 .build();
     }
 
